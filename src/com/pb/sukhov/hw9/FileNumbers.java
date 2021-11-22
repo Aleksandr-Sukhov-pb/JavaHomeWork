2b3022010f5368e616a216c8d8e651b40af4bb5a
Aleksandr Sukhov <aleksandr.sukhov@privatbank.ua>
2021-11-22 23:11:17 +0200

    init commit

--- src/com/pb/sukhov/hw9/FileNumbers.java
+++ src/com/pb/sukhov/hw9/FileNumbers.java
@@ -16,159 +16,143 @@ package com.pb.sukhov.hw9;
 
 import java.io.*;
 
-import java.io.FileOutputStream;
-import java.io.OutputStream;
-
 public class FileNumbers {
 
-    public static void print(String text) {                                                  //Метод void вывод на экран
+    public static void print(String text) {                                //Метод void вывод на экран
         System.out.println(text);
     }
 
-
-    static int generation() {
+    static int generation() {                                               //Метод генератор чисел от 1 до 99
         int gen = (int) (Math.random() * 100);
-        return gen;
+            if (gen<1) gen++;
+                return gen;
     }
 
-    void createOddNumbersFile() {
-        try (InputStream is = new FileInputStream("files/s01-input-text.txt")) {
+     public static int[] StringToMas(String str){                            //Метод конвертация строка в массив символов
+            String strArr[] = str.split(" ");
+        int numArr[] = new int[strArr.length];
+            for (int i = 0; i < strArr.length; i++) {
+                numArr[i] = Integer.parseInt(strArr[i]);
+          }
+            return numArr;
+    }
 
+    static void createOddNumbersFile() throws IOException {                 //читаем файл, обрабатываем, записываем в новый
+        String strOutFile = new String();
+        try (InputStream is = new FileInputStream("C:/Users/km110/IdeaProjects/JavaHomeWork/src/com/pb/sukhov/hw9/numbers.txt")) {
             int available = is.available();
-            byte[] buffer = new byte[available];
-            int i = 0;
-
+                byte[] buffer = new byte[available];
+                    int i = 0;
             int data = is.read();
-            while (data != -1) {
-                buffer[i] = (byte) data;
+                while (data != -1) {
+                    buffer[i] = (byte) data;
                 data = is.read();
-                i++;
-            }
-
-            System.out.println(new String(buffer));
+                    i++;
+                }
+            String strBuf = new String(buffer);
+//                              System.out.println(strBuf);                   //Вывод на экран для проверки
+            strOutFile = strBuf;
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-    }
+
+        String newString = strOutFile.replace('\n', ' ');      //заменяем переход на пробелы
+
+        int[] arrTemp = new int[100];
+        arrTemp = StringToMas(newString);                                     //вызов метода строка с пробелами в массив int
 
 
-    void createNumbersFile() throws IOException {
-        //   String data = "This is a line of text inside the file.";
+        int[] arrMasEven = new int[100];
 
-        int[] array = new int[200];
+        for (int i = arrTemp.length-1; i > 0;) {                            //блок разворот массива!!!
+            for (int j = 0; j < arrTemp.length; j++) {
+                arrMasEven[j] = arrTemp[i];
+                i--;
+            }
+        }
 
-        for (int n = 0; n < 199; n++) {
-            int gen = generation();
-            //  int t=n;
-            array[n] = gen;
-            n++;
-            array[n] = ' ';
-            n++;
-            if (n == 20 | n == 40 | n == 60 | n == 80 | n == 100 | n == 120 | n == 140 | n == 160 | n == 180) {
-                array[n] = '\n';
+        int[] arrMasEven2 = new int[100];
+
+           int i = 100;                                                      //блок создаем новый массив с четными 0-ми
+        while (i > 0) {
+            if ((i-1) % 2 == 0) {
+               arrMasEven2[i-1] = arrMasEven[i-1];
+               i--;
+           } else {
+                arrMasEven2[i-1] = 0;
+                i--;
             }
         }
 
 
-        OutputStream os = null;
+        File out = new File("C:/Users/km110/IdeaProjects/JavaHomeWork/src/com/pb/sukhov/hw9/odd-numbers.txt");  //путь к файлу
+        FileWriter fw = null;
+        int s = 100;
+
         try {
-            os = new FileOutputStream("files/s01-output-text.txt");
+            fw = new FileWriter(out);
+            BufferedWriter writer = new BufferedWriter(fw);
+            int line;
 
-            // Converts the string into bytes
-            //     byte[] dataBytes = data.getBytes();
-
-            // Writes data to the output stream
-            //      os.write(dataBytes);
-            System.out.println("Data is written to the file.");
-        } catch (Exception e) {
-            e.getStackTrace();
-        } finally {
-            if (os != null) {
-                try {
-                    os.close();
-                } catch (Exception ex) {
-                    ex.printStackTrace();
+            while (s > 0) {
+                line = arrMasEven2[s-1];
+                if ((s-1) % 10 == 0){
+                    writer.write(line + "\n");
+                    s--;
+                }
+                else { writer.write(line + " ");
+                    s--;
                 }
             }
+            writer.close();                                                     //закрываем поток
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
+    static void createNumbersFile() throws IOException {
+        int[] array = new int[100];
+            int n=100;
+                while (n > 0) {                                                    // создаем массив
+            int gen = generation();                                        // вызываем метод сл. числа 1-99
+                array[n-1] = gen;
+            n--;
+        }
 
-    //   int b = 37;
-    //      os.write(b); // записывает в выходной поток один байт, который представлен целочисленным параметром b
+        File out = new File("C:/Users/km110/IdeaProjects/JavaHomeWork/src/com/pb/sukhov/hw9/numbers.txt");  //путь к файлу
+            FileWriter fw = null;
+                int s = 100;
+        try {
+            fw = new FileWriter(out);
+                BufferedWriter writer = new BufferedWriter(fw);
+                    int line;
+                while (s > 0) {
+                    line = generation();
+                       if ((s-1) % 10 == 0){
+                    writer.write(line + "\n");
+                    s--;
+                }
+                else { writer.write(line + " ");
+                    s--;
+                }
+            }
+            writer.close();                                                     //закрываем поток
+        } catch (IOException e) {
+            e.printStackTrace();
 
-//        os.flush(); // очищает буфер вывода, записывая все его содержимое
-
-
-    //      byte[] masBytes = new byte[]{32, 0, 15, 12};
-    //     os.write(masBytes); // записывает в выходной поток массив байтов buffer.
-
-
-    //      byte[] buffer = new byte[]{32, 0, 15, 12, 1, 7, 78, 4};
-    //      int offset = 2;
-    //      int length = 3;
-    //      os.write(buffer, offset, length); // записывает в выходной поток некоторое число байтов, равное length,
-    //      // из массива buffer, начиная со смещения offset, то есть с элемента buffer[offset].
-
-    //     os.close(); // закрывает поток
-
+        }
+    }
 
     public static void main(String[] args) throws Exception {
         print(" ___________________________________ ");
         print("|  << F I L E     N U M B E R S >>  |");
         print("|___________________________________|");
 
-        int[] array = new int[101];
+        createNumbersFile();
+        createOddNumbersFile();
+        System.exit(0);
 
-        for (int n = 0; n < 101;) {
-            int gen = generation();
-            //  int t=n;
-            array[n] = gen;
-            n++;
-        }
-        System.out.print(" Массив чисел: ");  // "блок" вывод массива экран
-        for (int n = 0; n < 101; n++) {
-            System.out.print(array[n] + " ");
-            int m = n;
-            if (m % 10 == 0)
-                System.out.println(" ");
-            //  System.out.print(array[n] + "/n");
-        }
-            File out = new File("src/com/pb/sukhov/hw9/text.txt");
-            FileWriter fw = null;
+    }
 
-
-
-            int s = 100;
-            // Try block: Most stream operations may throw IO exception
-            try {
-                // Create file writer object
-                fw = new FileWriter(out);
-                // Wrap the writer with buffered streams
-                BufferedWriter writer = new BufferedWriter(fw);
-                int line;
-
-                while (s > 0) {
-                    // Randomize an integer and write it to the output file
-                    line = generation();
-                    writer.write(line + " ");
-                  //  int m=s;
-                    if (s % 10 == 0)
-                        writer.write(line + "\r\n");
-
-                    s--;
-                }
-                // Close the stream
-                writer.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-                System.exit(0);
-            }
-        }
-
-
-
-        }
-
-
+}
 
